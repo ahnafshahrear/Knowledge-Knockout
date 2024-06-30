e1edc0fefdf575e9c040b4a6e43641aab7f721cd
Ahnaf Shahrear Khan <ahnafshahrearkhan@gmail.com>
2024-07-01 02:07:02 +0600

    Image added

--- src/Rules.java
+++ src/Rules.java
@@ -12,45 +12,40 @@ public class Rules extends JFrame implements ActionListener{
         getContentPane().setBackground(Color.WHITE);
         setLayout(null);
 
-        JLabel heading = new JLabel("Welcome " + name + " to Knowledge Knockout");
-        heading.setBounds(50, 20, 700, 30);
+        JLabel heading = new JLabel("Welcome to Knowledge Knockout");
+        heading.setBounds(180, 20, 700, 30);
         heading.setFont(new Font("Viner Hand ITC", Font.BOLD, 28));
         heading.setForeground(new Color(30, 144, 254));
         add(heading);
 
         JLabel rules = new JLabel();
-        rules.setBounds(20, 90, 700, 350);
+        rules.setBounds(80, 40, 700, 350);
         rules.setFont(new Font("Tahoma", Font.PLAIN, 16));
         rules.setText(
                 "<html>"+
-                        "1. You are trained to be a programmer and not a story teller, answer point to point" + "<br><br>" +
-                        "2. Do not unnecessarily smile at the person sitting next to you, they may also not know the answer" + "<br><br>" +
-                        "3. You may have lot of options in life but here all the questions are compulsory" + "<br><br>" +
-                        "4. Crying is allowed but please do so quietly." + "<br><br>" +
-                        "5. Only a fool asks and a wise answers (Be wise, not otherwise)" + "<br><br>" +
-                        "6. Do not get nervous if your friend is answering more questions, may be he/she is doing Jai Mata Di" + "<br><br>" +
-                        "7. Brace yourself, this paper is not for the faint hearted" + "<br><br>" +
-                        "8. May you know more than what John Snow knows, Good Luck" + "<br><br>" +
+                        "There are only 2 rules:" + "<br><br>" +
+                        "1. You can't give any wrong answer" + "<br><br>" +
+                        "3. You'll get 15 seconds for each questions" + "<br><br>" +
                         "<html>"
         );
         add(rules);
 
         back = new JButton("Back");
-        back.setBounds(250, 500, 100, 30);
+        back.setBounds(280, 500, 100, 30);
         back.setBackground(new Color(30, 144, 254));
         back.setForeground(Color.WHITE);
         back.addActionListener(this);
         add(back);
 
         start = new JButton("Start");
-        start.setBounds(400, 500, 100, 30);
+        start.setBounds(420, 500, 100, 30);
         start.setBackground(new Color(30, 144, 254));
         start.setForeground(Color.WHITE);
         start.addActionListener(this);
         add(start);
 
-        setSize(800, 650);
-        setLocation(350, 100);
+        setSize(820, 680);
+        setLocation(360, 60);
         setVisible(true);
     }
 
